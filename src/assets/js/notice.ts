e44388ad498062d3a924552e0b61ee5da7d123d1
首都爱护动物协会 <87239270+1355873789@users.noreply.github.com>
2025-03-05 03:57:29 +0800

    u7

--- src/assets/js/notice.ts
+++ src/assets/js/notice.ts
@@ -29,7 +29,7 @@ export interface NoticeTopResponse {
 
 export const fetchNotice = async (): Promise<NoticeResponse['data'] | null> => {
   try {
-    const response = await fetch('http://103.40.13.68:18192/items/notice', {
+    const response = await fetch('https://cherry-data.ocoolai.com/items/notice', {
       method: 'GET'
     });
     const data: NoticeResponse = await response.json();
@@ -42,7 +42,7 @@ export const fetchNotice = async (): Promise<NoticeResponse['data'] | null> => {
 
 export const fetchTopNotice = async (): Promise<NoticeTopResponse['data'] | null> => {
   try {
-    const response = await fetch('http://103.40.13.68:18192/items/notice_top', {
+    const response = await fetch('https://cherry-data.ocoolai.com/items/notice_top', {
       method: 'GET'
     });
     const data: NoticeTopResponse = await response.json();
